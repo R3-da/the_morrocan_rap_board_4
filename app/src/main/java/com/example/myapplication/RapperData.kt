2c6149e5785c7e068fd2054a36494a3dfc082a56
haddanreda <haddan.reda.net@gmail.com>
2022-03-10 23:16:35 +0100

    daily commit added a comment on the rappersdata class

--- app/src/main/java/com/example/myapplication/RapperData.kt
+++ app/src/main/java/com/example/myapplication/RapperData.kt
@@ -1,4 +1,4 @@
 package com.example.myapplication
 
-
+//i maaybe need to add some more information for future filter implementing like on wich generation each rapper belongs
 data class RapperData(val name:String, val bg:Int, val ic:Int, val adlibs:MutableList<String>)
